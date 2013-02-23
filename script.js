86c27a7efc4fc6294af3a40f556cf39fd3b79b4a
Jérémie Astori <jeremie@astori.fr>
2013-02-23 23:48:18 +0100

    cleanup wind in current scene

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
 
-  var currentScene = 'none'; // sirene
+  function launchFullScreen(element) {
+    if(element.requestFullScreen)
+      element.requestFullScreen();
+    else if(element.mozRequestFullScreen)
+      element.mozRequestFullScreen();
+    else if(element.webkitRequestFullScreen)
+      element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+  }
+
+  var currentScene = 'none'; // sirene - wind
+  var playing = false;
 
   if (currentScene == "sirene") {
 
@@ -17,16 +27,6 @@ $(document).ready(function() {
     });
   }
 
-  function launchFullScreen(element) {
-    if(element.requestFullScreen)
-      element.requestFullScreen();
-    else if(element.mozRequestFullScreen)
-      element.mozRequestFullScreen();
-    else if(element.webkitRequestFullScreen)
-      element.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
-  }
-
-  var playing = false;
 
   $("#canvas").hide();
 
@@ -49,67 +49,70 @@ $(document).ready(function() {
     $("#canvas").css("height", w*9/16 + "px"); 
   });
 
-var FPS = 30;
-setInterval(function() {
-  update();
-  draw();
-}, 1000/FPS);
+  var FPS = 30;
+  setInterval(function() {
+    update();
+    draw();
+  }, 1000/FPS);
 
-function Cloud(I) {
-  I.x = 0;
-  I.y = 0;
+  function Cloud(I) {
+    I.x = 0;
+    I.y = 0;
 
-  I.width  = 100;
-  I.height = 100;
+    I.width  = 100;
+    I.height = 100;
 
-  I.draw = function() {
-    
-    $("#canvas").drawImage({
-      source: "nuage2.png",
-      x: this.x,
-      y: this.y
-    });
-  };
+    I.draw = function() {
+      
+      $("#canvas").drawImage({
+        source: "nuage2.png",
+        x: this.x,
+        y: this.y
+      });
+    };
 
-  I.update = function() {
- //   I.x += I.xVelocity;
- //   I.y += I.yVelocity;
+    I.update = function() {
+   //   I.x += I.xVelocity;
+   //   I.y += I.yVelocity;
 
-    I.x += 20;
-    I.x %= 1600;
-  };
+      I.x += 20;
+      I.x %= 1600;
+    };
 
-  return I;
-}
+    return I;
+  }
 
 
-// Scène vent
-var clouds = []
+  // Scène vent
+  var clouds = []
 
-clouds.push(Cloud({x: 50}));
-
-clouds.forEach(function(cloud) {
-  cloud.update();
-});
-
-function update() {
-  if(!playing) return;
+  clouds.push(Cloud({x: 50}));
 
   clouds.forEach(function(cloud) {
     cloud.update();
   });
 
-}
+  function update() {
+    if(!playing) return;
 
-function draw() {
-  if(!playing) return;
+    if(currentScene == "wind") {
+      clouds.forEach(function(cloud) {
+        cloud.update();
+      });
+    }
 
-  $("canvas").clearCanvas();
+  }
 
+  function draw() {
+    if(!playing) return;
 
-  clouds.forEach(function(cloud) {
-    cloud.draw();
-  });
+    $("canvas").clearCanvas();
+
+    if(currentScene == "wind") {
+      clouds.forEach(function(cloud) {
+        cloud.draw();
+      });
+    }
 }
 
 });
